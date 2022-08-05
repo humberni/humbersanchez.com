efb8be853414209c07329e1587c481600e2f23ef
Humber Sanchez Nieto <husani925@gmail.com>
2022-08-05 12:33:19 -0500

    agregando google analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
